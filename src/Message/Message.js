61d9f557ee3c213300cdcd9f1d29738366f765d9
Juha Wilppu <juha.wilppu@gmail.com>
2017-11-29 23:31:12 +0200

    Add PropTypes for Message

--- src/Message/Message.js
+++ src/Message/Message.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import WithClass from '../hoc/WithClass';
+import PropTypes from 'prop-types';
 
 const message = (props) => {
     return (
@@ -8,5 +9,14 @@ const message = (props) => {
         </WithClass>
     );
 }
+
+//  PropTypes do not work for functional components, but this is how they would look like...
+message.PropTypes = {
+    from: PropTypes.string,
+    message: PropTypes.func,
+    order: PropTypes.number,
+    click: PropTypes.func
+
+};
 
 export default message;
