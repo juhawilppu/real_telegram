07b6feb35b023c567d7ea81360c5c4774075c6f3
Juha Wilppu <juha.wilppu@gmail.com>
2017-12-10 22:07:48 +0200

    Fix broken test

--- src/MessageInput/MessageInput.test.js
+++ src/MessageInput/MessageInput.test.js
@@ -8,7 +8,7 @@ configure({ adapter: new Adapter() });
 
 describe('<MessageInput />', function () {
   it('should render without crashing', function () {
-    send = (message) => console.log(message);
+    const send = (message) => console.log(message);
     const wrapper = shallow(<MessageInput send={send}/>);
     expect(wrapper).to.have.length(1);
   });
